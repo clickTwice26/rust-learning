66cb0861c4852f95a243ef86a10ea52882df7afb
Shagato <47025267+clickTwice26@users.noreply.github.com>
2026-02-19 02:14:34 +0600

    chore: update .gitignore and readme, refactor variable usage
    Add .DS_Store to gitignore to ignore macOS system files.
    Update readme with new video title.
    Refactor main.rs to use inline variable initialization and add a constant.

--- Readme.md
+++ Readme.md
@@ -1,7 +1,11 @@
 
 
 
-#Video 2
+# Video 2
 Title: Programming a guessing game | Rust book
+
+# Video 3
+Title : Variables and Mutability
+
 
 
--- variables_learn/src/main.rs
+++ variables_learn/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    let mut age;
-    age = 25;
-    age = 24;
+    let mut age: i32 = 24;
     println!("My age is {age}");
+    const PI : f64 = 3.14;
+    age = 3;
+    println!("Update age is {age}, Value of PI : {PI}");
+    // const THREE_HOURS_IN_SECONDS : u32 = 3600* age; //wrong cant use non-constant value in a constant variable
+
 }
