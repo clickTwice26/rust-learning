068eb0e82e0ee69e0b1e7278b3901e8f7743527d
Shagato <47025267+clickTwice26@users.noreply.github.com>
2026-02-27 21:32:53 +0600

    learned function with multiple use type of return

--- learning_functions/src/main.rs
+++ learning_functions/src/main.rs
@@ -1,12 +1,33 @@
 // Learning functions
 
-
-fn main() {
-    let greeting : &str = "hello";
-    // rust compiler will run only the main function
-    println!("{greeting}, world!");
-    another_function(5, true); // 5 is the argument
+fn main(){
+    // let y: i32 = add(5, 6); 
+    let y = is_even(4);
+    println!("Value of y is {y}");
 }
+fn is_even(x : i32) -> bool{
+    if x % 2 == 0{
+        return true;
+    }
+    println!("The number is not even");
+    false
+}
+fn add(x : i32 , y : i32) -> i32{
+    // return 5;
+    // 5
+    // x + y
+    let result = x + y;
+    result
+}
+
+// fn main() {
+//     let greeting : &str = "hello";
+//     // rust compiler will run only the main function
+//     println!("{greeting}, world!");
+//     another_function(5, true); // 5 is the argument
+// }
+
+
 fn another_function(x: i32, y : bool) {  
     // x is parameter
     // must have to annotate the types of the parameter
