2a5fa0c4bb318bb53ba23be51e4b921f5d671718
Shagato <47025267+clickTwice26@users.noreply.github.com>
2026-02-27 20:43:32 +0600

    docs: add data_types documentation placeholder and explain function concepts
    Add initial placeholder file for data_types documentation. Extend the example in learning_functions to include an `add_function` and demonstrate control flow with an `if` statement. Include explanatory comments about statements vs expressions, parameter type annotations, and function calls.

--- learning_functions/src/main.rs
+++ learning_functions/src/main.rs
@@ -7,7 +7,26 @@ fn main() {
     println!("{greeting}, world!");
     another_function(5, true); // 5 is the argument
 }
-fn another_function(x: i32, y : bool) {  //x is parameter
+fn another_function(x: i32, y : bool) {  
+    // x is parameter
+    // must have to annotate the types of the parameter
 
     println!("Hello from another_function(), {x} {y}");
+    if y == true {
+        println!("Yes it is true");
+    }
+    add_function(1, 3);
+}
+fn add_function(x : i32, y : i32){
+    // Statement vs Expression
+    let add : i32 = x + y;
+
+    // let add : i32 = x + y; this is a statement; statement do not return values, expressions are the parts of statement
+    // calling a function is an expression and macro also expression
+    // but function
+    // x + y this is an expression
+    println!("Total : {x} + {y} = {add}");
+    println!("{:?}", add);
+
+
 }
