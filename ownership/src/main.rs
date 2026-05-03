0b86d48f2b02f97eaa579c580ef069e10e801eba
clickTwice26 <shagatoc@gmail.com>
2026-05-03 21:07:05 +0600

    feat: add functions to demonstrate ownership concepts
    Add `takes_ownership`, `gives_ownership`, and `takes_and_gives_ownership` functions to illustrate Rust's ownership rules with String data. Remove previous variable examples to focus on function-based ownership transfer.

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,16 +1,28 @@
 fn main() {
 
-    let mut x = 5;
-    let y = x;
-
-    let hello: String = String::from("I am hello");
-    println!("hello is {hello}");
-    let world = hello.clone();
-    println!("World is {hello}");
-    println!("World is {world}");
-    // println!("X is {x} and y is {y}");
+    let num = 10;
+    let result = add(num);
+    let name = String::from("Shagato");
+    takes_ownership(name);
+    // print!("Vale of name is {name}");
+    println!("Num is {num}\nResult is {result}");
+    let s = gives_ownership();
+    println!("{s}");
+    let s2 = takes_and_gives_ownership(s);
+    println!(" s2 = {s2}");
 }
 
 fn add(x : i32) -> i32{
     x + 10
+}
+fn takes_ownership(s : String){
+    println!("Inside ownership : {s}");
+}
+fn gives_ownership() -> String{
+    let s = String::from("This is a string from gives_ownership function");
+    s
+}
+fn takes_and_gives_ownership(s :String) -> String{
+    println!(" S in takes_and_gives_ownership = {s}");
+    s
 }
