f194c3d0d145051074a650b5f30ede708ed9b06e
clickTwice26 <shagatoc@gmail.com>
2026-05-03 22:36:19 +0600

    refactor(ownership): change calculate_length to borrow string
    Instead of taking ownership of the String and returning it, the function now borrows a reference. This avoids unnecessary moves and cloning, making the code more efficient and idiomatic.

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
     let name : String = String::from("Shagato Chowdhury");
-    let (name, l) = calculate_length(name);
+    let l : usize = calculate_length(&name);
     println!("The length of string `name`: {name} is {l}");
 
 }
 
 
-fn calculate_length(s : String) -> (String, usize){
+fn calculate_length(s : &String) -> usize{
     let result: usize = s.len();
-    return (s, result);
+    return result;
 }
