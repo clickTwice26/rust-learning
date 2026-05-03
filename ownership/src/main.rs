2b15a885d673aa32bb574d8066a0bafe1c985f2e
clickTwice26 <shagatoc@gmail.com>
2026-05-03 22:30:06 +0600

    refactor(ownership): simplify main with tuple return from calculate_length
    Remove unused functions and demonstrate ownership transfer by returning
    a tuple containing the original string and its length.

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,28 +1,12 @@
 fn main() {
+    let name : String = String::from("Shagato Chowdhury");
+    let (name, l) = calculate_length(name);
+    println!("The length of string `name`: {name} is {l}");
 
-    let num = 10;
-    let result = add(num);
-    let name = String::from("Shagato");
-    takes_ownership(name);
-    // print!("Vale of name is {name}");
-    println!("Num is {num}\nResult is {result}");
-    let s = gives_ownership();
-    println!("{s}");
-    let s2 = takes_and_gives_ownership(s);
-    println!(" s2 = {s2}");
 }
 
-fn add(x : i32) -> i32{
-    x + 10
-}
-fn takes_ownership(s : String){
-    println!("Inside ownership : {s}");
-}
-fn gives_ownership() -> String{
-    let s = String::from("This is a string from gives_ownership function");
-    s
-}
-fn takes_and_gives_ownership(s :String) -> String{
-    println!(" S in takes_and_gives_ownership = {s}");
-    s
+
+fn calculate_length(s : String) -> (String, usize){
+    let result: usize = s.len();
+    return (s, result);
 }
