5088c35b8104be171e22aaca50b33b3e4059a766
clickTwice26 <shagatoc@gmail.com>
2026-05-07 20:05:32 +0600

    refactor(ownership): change calculate_length to take mutable reference
    Modify the function to accept a mutable reference to a String to allow
    appending text within the function. This demonstrates mutable borrowing
    in Rust's ownership system.

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,12 +1,15 @@
 fn main() {
-    let name : String = String::from("Shagato Chowdhury");
-    let l : usize = calculate_length(&name);
+    let mut name : String = String::from("Shagato Chowdhury");
+    let l : usize = calculate_length(&mut name);
     println!("The length of string `name`: {name} is {l}");
 
 }
 
 
-fn calculate_length(s : &String) -> usize{
+fn calculate_length( s : &mut String) ->usize{
+    s.push_str("Hello world");
     let result: usize = s.len();
-    return result;
+    println!("Inside the function");
+    println!("{}", *s);
+    return result
 }
